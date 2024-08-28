d342a2f2733cd341aefc31ceefa574238040dc81
Kajal <115399536+KajalSingh19@users.noreply.github.com>
2024-08-28 19:17:37 +0530

    Update ArrayAssign.java

--- WIPRO Assignments/ArrayAssign.java
+++ WIPRO Assignments/ArrayAssign.java
@@ -1,32 +1,52 @@
 import java.util.*;
 public class ArrayAssign {
 
-public static void findElement(int[]  arr, int key){    //Assign 3
+public static void findElement(int[]  arr, int key){
+    boolean found = false;
     for(int i = 0; i < arr.length; i++){
+    
         if(arr[i] == key){
+            found = true;
             System.out.println(i);
             break;
-            
         }
-        else {
-            System.out.println("-1");
+        
+    }
+    if(!found){
+        System.out.println("-1");
+
+    }
+}
+
+public static void NoDuplicate(int[] arr){
+    List<Integer> distinctArr = new ArrayList<>() ;
+    for(int ele : arr){
+        if( !distinctArr.contains(ele)){
+            distinctArr.add(ele);
         }
     }
+    System.out.println(Arrays.toString(distinctArr.toArray()));
 }
     public static void main(String[] args) {
        
         Scanner sc = new Scanner(System.in);
-       int arr[] = {12, 123,34,67,1};
+       int arr[] = {12, 123,34,67,1,1,34};
+        //Assign 3
+System.out.println("the index of the element to be found is ");  
+  findElement(arr, 12);
 
-        //Assign 2
-             Arrays.sort(arr);
+        //Assign 7
+  NoDuplicate(arr);
+             
+
+             ///Assign 2
+        Arrays.sort(arr);
              System.out.println("Min ele in the array "+ arr[0] );
             System.out.println("Max ele in the array "+arr[arr.length -1]);
 
             
-  System.out.println("the index of the element to be found is ");  
-  findElement(arr, 12);          
+  
+  
 sc.close();
     }
 }
-
